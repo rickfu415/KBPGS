8742ed3e5103855e2de27fe7c4bee5dc821c11d0
coolfurui <coolfurui@gmail.com>
2023-09-22 15:50:46 -0400

    Create function for running single game

---
+++ singleGame.py
@@ -0,0 +1,23 @@
+"""
+Kentucky Basketball Pickup Game Simulator
+
+Author: Rui(Rick) Fu
+Date: 09-22-2023
+"""
+
+
+import player
+import game
+import team
+
+
+def single_game_run():
+
+
+    return
+
+
+if __name__ == "__main__":
+    # default as two teams, with 5 players
+    # Game Rule (2,3,30) or (1,2,15)
+    single_game_run()
