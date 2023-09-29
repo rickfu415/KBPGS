bf48362bbe0816b685502f9c01e41fa534f89208
coolfurui <coolfurui@gmail.com>
2023-09-28 22:42:31 -0400

    Now the offense oppurtunity is evenly distributed among all players. Should be a dynamic model. Will be improved

--- team.py
+++ team.py
@@ -4,7 +4,10 @@ Kentucky Basketball Pickup Game Simulator
 Author: Rui(Rick) Fu
 Date: 09-22-2023
 """
+
+import random
 import player
+import utility
 
 
 class Team():
@@ -13,4 +16,30 @@ class Team():
         self.ID = ID
         self.status = "None" # either in Offense or Defense
         self.current_score = 0
-        
+        self.player_list = []
+        self.player_weights = [0.2, 0.2, 0.2, 0.2, 0.2] # needs to be based on capability
+        self.offense_probability_range = None
+    
+    def assemble(self): # call after all players are added
+        self.player_weights = [0.2, 0.2, 0.2, 0.2, 0.2] # needs to be based on capability
+        self.offense_probability_range = utility.weighted_sections(self.player_weights)
+        print("Team",self.Name,"has",len(self.player_list),"players")
+        print("Team",self.Name,"offense_probability_range",self.offense_probability_range)
+
+
+
+    def add_player(self, p):
+        self.player_list.append(p)
+        if len(self.player_list)>5:
+            print("Team",self.Name,"has More than 5 player!")
+    
+    def offense(self):
+        which_player_to_offense = utility.find_range(random.uniform(0, 1), self.offense_probability_range)
+        result = self.player_list[which_player_to_offense].offense()
+        if (result==-1):
+            #print("Team",self.Name,"had a turnover")
+            return
+        if (result>0):
+            self.current_score += result
+            #print("Team",self.Name,"score now is",self.current_score)
+        return
